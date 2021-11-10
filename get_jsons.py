7a59f28bf869798b9df2ffd864be36a589c08cd7
DurbeKK <k2.durbek@gmail.com>
2021-11-10 23:10:51 +0500

    Gets group name in all caps now
    Now it should work, i guess

--- get_jsons.py
+++ get_jsons.py
@@ -66,7 +66,7 @@ select = Select(
 )
 
 # this is just a list of all group names
-all_groups = [option.text.upper() for option in select.options]
+all_groups = [option.text for option in select.options]
 
 # the first 2 elements in all_groups array are an empty blank and '1 time'
 for group in all_groups[2:]:
@@ -169,6 +169,8 @@ for group in all_groups[2:]:
                 last_class["length"] += 1.0
             else:
                 days[day].append(ready_details)
+
+    group = group.upper()
 
     # obtaining the course name (e.g. 5BIS, 6ECwF, etc.)
     course_regex = r"[3-6]\D+"
